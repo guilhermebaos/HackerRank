2d3fc723ea40714915b81fd4deee4995b129d384
Guilherme Silva <guilhermebaos@gmail.com>
2020-06-07 21:50:23 +0100

    Solved Sherlock and Anagrams

--- Strings/Medium/001_Sherlock_And_Anagrams.py
+++ Strings/Medium/001_Sherlock_And_Anagrams.py
@@ -2,10 +2,17 @@ def sherlockAndAnagrams(s):
     if unique(s):
         return 0
 
-    s_list = []
-    for letter in s:
-        s_list += [letter]
+    anagrammatic_pairs = ap = 0
 
+    for num_letters in range(1, len(s)):
+        slices = []
+        for c in range(0, len(s) - num_letters + 1):
+            cut = s[c:c + num_letters]
+            cut = sort_string(cut)
+            if slices.__contains__(cut):
+                ap += slices.count(cut)
+            slices += [cut]
+    return ap
 
 
 def unique(string):
@@ -16,3 +23,17 @@ def unique(string):
         else:
             string_list += [letter]
     return True
+
+
+def sort_string(string):
+    s_list = []
+    for letter in string:
+        s_list += [letter]
+    s_list.sort()
+    new_s = ''
+    for letter in s_list:
+        new_s += letter
+    return new_s
+
+
+print(sherlockAndAnagrams('kkkk'))
