bcaa68a2625e61b465090a000f38e088519ce76e
Guilherme Silva <guilhermebaos@gmail.com>
2020-07-15 13:29:59 +0100

    Solved BigSorting

---
+++ Algorithms/Sorting/Easy/010_Big_Sorting.py
@@ -0,0 +1,30 @@
+def bigSorting(unsorted): return bigQuickSortStrings(unsorted)
+
+
+def bigQuickSortStrings(arr):
+    if len(arr[:2]) <= 1:
+        return arr
+
+    pivot = arr[0]
+    len_pivot = len(pivot)
+    int_pivot = int(pivot)
+
+    left, equal, right = [], [pivot], []
+    for num in arr[1:]:
+        len_num = len(num)
+        if len_num > len_pivot:
+            right += [num]
+        elif len_num < len_pivot:
+            left += [num]
+        else:
+            int_num = int(num)
+            if int_num < int_pivot:
+                left += [num]
+            elif int_num > int_pivot:
+                right += [num]
+            else:
+                equal += [num]
+
+    left = bigQuickSortStrings(left)
+    right = bigQuickSortStrings(right)
+    return left + equal + right
